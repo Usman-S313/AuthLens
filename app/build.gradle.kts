18839066cd392c5be67c31d60ab94ad089f8c889
Usman Siddique <usman098756@gmail.com>
2026-06-26 03:18:34 -0700

    Configure custom APK naming for release builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,14 @@ android {
         }
     }
 
+    android.applicationVariants.all {
+        val variant = this
+        variant.outputs.all {
+            val output = this as? com.android.build.gradle.internal.api.BaseVariantOutputImpl
+            output?.outputFileName = "AuthLens-${variant.name}-${variant.versionName}.apk"
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
